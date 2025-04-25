940f7beccfd55613f9069791e879dc80d7a82659
Hugs-4-Bugs <prabhathkumar.cse@skit.org.in>
2025-04-25 07:06:26 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
+import  react  from "@vitejs/plugin-react";
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/PrabhatVerse",
+  base: process.env.VITE_BASE_PATH || "/",
     server: {
     host: true,       // Make server externally accessible
     port: 5173,       // Correct port
@@ -14,3 +17,26 @@ export default defineConfig({
   },
 });
 
+
+
+
+
+
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";  // Correct import statement
+
+// export default defineConfig({
+//   plugins: [react()],  // Use the react plugin
+//   base: process.env.VITE_BASE_PATH || "/PrabhatVerse",
+//   server: {
+//     host: true,       // Make server externally accessible
+//     port: 5173,       // Correct port
+//     headers: {
+//       "ngrok-skip-browser-warning": "true",  // Skip Ngrok browser warning page
+//     },
+//   },
+// });
